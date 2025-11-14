26eab72b0ea53db57472c5f7533a0c98fa5db631
kdutt0910 <kdutt000@gmail.com>
2025-11-14 10:25:17 +0000

    Datatype

---
+++ datatypes/datatype.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//alert( 3 + 3 ) we are using node js not browser
+
+let name = "Kaushik"
+let age = 26
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null =>standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof name);
---
+++ variables/variable.js
@@ -0,0 +1,12 @@
+const accountId = 12154
+let accountEmail = "kdutt0910@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "kd@gmail.com"
+accountPass = "0987"
+accountCity = "Patna"
+
+console.log(accountId,accountEmail,accountPass,accountCity,accountState);
+console.table([accountId, accountEmail, accountPass, accountCity,accountState])
